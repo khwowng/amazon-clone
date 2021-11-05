2be15637ea45f7d3e931b9f6baf8d426fa8873b5
khwowng-ha <khuongha.812@gmail.com>
2021-11-05 18:54:58 +0100

    Set Up Files

---
+++ src/components/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "./Header";
+
+
+function Checkout() {
+    return (
+      <div>
+        <Header />
+        checkout
+      </div>
+    );
+}
+
+export default Checkout
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
+function Header() {
+    return (
+        <div>
+            header
+        </div>
+    )
+}
+
+export default Header
