0c88f29f267dcc6eec6bca0becbab5a0e42d289a
khwowng-ha <khuongha.812@gmail.com>
2021-11-09 14:17:24 +0100

    Update Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function Login() {
-    return (
-        <div>
-            login
-        </div>
-    )
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault(); // prevent page from refreshing
+    // firebase login
+  };
+  const register = (e) => {
+    e.preventDefault(); // prevent page from refreshing
+    // firebase register
+  };
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-in</h1>
+
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              type="email"
+              value={password}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button type="submit" variant="warning" onClick={signIn}>
+            Sign In
+          </Button>
+        </Form>
+
+        <p className="text-muted">
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        <Button variant="light" onClick={register}>
+          Create your Amazon Account
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
