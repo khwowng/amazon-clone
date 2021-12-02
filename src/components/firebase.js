f40eb30edad8f116d8b7df0af6ae0e25b8b21d2b
khwowng <khuongha.812@gmail.com>
2021-12-02 12:26:15 +0100

    Version Update

---
+++ src/components/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAUWIjv_uvJlQm_l3KKrs6532zwm8d1EYI",
+  authDomain: "clone-a3edf.firebaseapp.com",
+  projectId: "clone-a3edf",
+  storageBucket: "clone-a3edf.appspot.com",
+  messagingSenderId: "904476827946",
+  appId: "1:904476827946:web:5953b3febb95ede5e83e53",
+  measurementId: "G-K4TC6RKHPP",
+};
+
+// Initialize Firebase
+// const analytics = getAnalytics(app);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
